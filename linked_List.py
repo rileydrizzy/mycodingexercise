11f06d924a3b6c7161890f04eb83706373b11d1c
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-03-18 03:51:24 +0100

    adding task todo

--- linked_List.py
+++ linked_List.py
@@ -1,5 +1,9 @@
+# write introduction to linked list
+#Talk about the Big O notation in respect to the key operation of a data structure (reading, searching, insertion, deletion)
+# list the advantages and disadvantages of linked list with example of areas of used
 
-#
+
+
 
 class Node:
     def __init__(self, data):
