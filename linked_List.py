6cb26b2d357f7c13255eead2ba13ae734ae6dcd2
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-03-18 06:33:53 +0100

    creating the append method

--- linked_List.py
+++ linked_List.py
@@ -1,7 +1,7 @@
 # write introduction to linked list
 #Talk about the Big O notation in respect to the key operation of a data structure (reading, searching, insertion, deletion)
 # list the advantages and disadvantages of linked list with example of areas of used
-
+# do the same for Double Linked List, Circual Linked List
 
 
 
@@ -18,6 +18,35 @@ class SingleLinkedList:
         self.sizelen = 0
 
     def append(self, newdata, pos= None):
-        if pos == None:
+         if pos == None:
             node = Node(newdata)
             self.Tail 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Test cases
+def test_case(Node):
+    Node('Spam')
+    Node('Eggs')
+    Node('Ham')
+
+#test_case(Node)
