6b561cc28a5e6513bf2d60edfb1f125e2ec18d7f
Ipadeola Ladipo Ezekiel <105759894+rileydrizzy@users.noreply.github.com>
2023-03-27 14:15:20 +0000

    Updated linked list

--- linked_list.py
+++ linked_list.py
@@ -1,63 +1,268 @@
-# write introduction to linked list
-#Talk about the Big O notation in respect to the key operation of a data structure (reading, searching, insertion, deletion)
-# list the advantages and disadvantages of linked list with example of areas of used
-# do the same for Double Linked List, Circual Linked List
-#develop and run test cases 
+'''
+A Linked list is a 
+'''
+
+# Node
 
 
 class Node:
-    def __init__(self, data = None):
+    '''
+    node to 
+    '''
+
+    def __init__(self, data=None):
         self.data = data
         self.next = None
-    
-class SingleLinkedList:
+        self.prev = None
+
+# Single Linked List
+
+
+class SingleList:
+    '''
+    Single List
+    '''
+
     def __init__(self):
-        self.Head = None
-        self.Tail = None
-        self.sizelen = 0
+        self.head = None
+        self.tail = None
+        self.size = 0
 
-    def append(self, newdata, index = None):
-        
-        if index == None:
-            new_node = Node(newdata)
-            if self.Tail:
-                self.Tail.next = new_node
+    def add(self, data, index=None):
+        '''
+        add
+        '''
+        node = Node(data)
+        if index is None:  # add to the list if there are no elment in it
+            if self.head is None:
+                self.head = node
+                self.tail = node
+                self.size += 1
+            else:  # adds to the tail
+                self.tail.next = node
+                self.tail = node
+                self.size += 1
+        else:
+            if index == 0:
+                node.next = self.head
+                self.head = node
+                self.size += 1
+            elif index == (self.size - 1):
+                self.tail.next = node
+                self.tail = node
+                self.size += 1
+
+            else:  # adding to specified index
+                current = self.head
+                previous = self.head
+                current_pointer_index = 0
+                while current:
+                    if index == current_pointer_index:
+                        node.next = current
+                        previous.next = node
+                        self.size += 1
+                        return
+                    previous = current
+                    current = current.next
+                    current_pointer_index += 1
+                print("The index is out of bounds")
+
+    def trasveral(self):
+        '''
+        trasveral
+        '''
+        current = self.head
+        while current:
+            value = current.data
+            current = current.next
+            yield value
+
+    def list_size(self):
+        '''
+        retrun the size of list
+        '''
+        return self.size
+
+    def seacrh(self, data):
+        '''
+        search for a data
+        '''
+        current = self.head
+        while current:
+            if data == current.data:
+                return True
             else:
-                self.Tail, self.Head = new_node
-            self.sizelen += 1
-    elif:
+                current = current.next
+        return False
+
+    def remove(self, data):
+        '''
+        removes
+        '''
+        current = self.head
+        previous = self.head
+        while current:
+            if current.data == data:
+                current = current.next
+                previous.next = current
+                self.size -= 1
+                return
+            previous = current
+            current = current.next
+        print('Data not in list')
 
 
-current_index = 0 
-while current_index != index:
-    
+# TEST CASES
+sample = ['ladi', 'liver', 'cool', 'breeze', 'nice', 'cat', 'dog']
+Singlelist = SingleList()
+for word in sample:
+    Singlelist.add(word)
+
+for letter in Singlelist.trasveral():
+    print(letter)
+print(Singlelist.list_size())
+
+Singlelist.add('Make', 3)
+Singlelist.add('out', 5)
+
+for letter in Singlelist.trasveral():
+    print(letter)
+print(Singlelist.list_size())
+
+Singlelist.remove('ot')
+
+for letter in Singlelist.trasveral():
+    print(letter)
+print(Singlelist.list_size())
 
 
+# DOUBLE LINKED LIST
 
 
+class DoubleList:
+    '''
+    explain double
+    '''
+
+    def __init__(self):
+        '''
+        explain
+        '''
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def add(self, data, index=None):
+        '''
+        explain
+        '''
+        node = Node(data)
+        if index is None:
+            if self.head is None:
+                self.head = node
+                self.tail = node
+                self.size += 1
+            else:
+
+                self.tail.next = node
+                node.prev = self.tail
+                self.tail = node
+                self.size += 1
+        elif index == 0:
+            current_head = self.head
+            current_head.prev = node
+            node.next = current_head
+            self.head = node
+            self.size += 1
+
+        elif index == (self.size - 1):
+            current_tail = self.tail
+            current_tail.next = node
+            node.prev = current_tail
+            self.tail = node
+            self.size += 1
+
+        else:
+            current_pointer = self.head.next
+            previous_pointer = self.head
+            current_pointer_index = 1
+            while current_pointer:
+                if index == current_pointer_index:
+                    previous_pointer.next = node
+                    node.prev = previous_pointer
+                    node.next = current_pointer
+                    current_pointer.prev = node
+                    self.size += 1
+                    return
+                else:
+                    previous_pointer = current_pointer
+                    current_pointer = current_pointer.next
+                    current_pointer_index += 1
+            print('Out of bound')
+
+    def seacrh(self, data):
+        '''
+        search
+        '''
+        current = self.head
+        while current:
+            if data == current.data:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def list_size(self):
+        '''
+        size
+        '''
+        return self.size
+
+    def trasvesral(self):
+        '''trasversal'''
+        current = self.head
+        while current:
+            value = current.data
+            current = current.next
+            yield value
+
+    def reversal(self):
+        '''
+        rev
+        '''
+        current_tail = self.tail
+        while current_tail:
+            value = current_tail.data
+            current_tail = current_tail.prev
+            yield value
+
+    def remove(self, data):
+        '''
+        remove
+        '''
+        pass
 
 
+# TEST CASE AREA
+sample = ['dipo', 'james', 'simi', 'ladi', 'biggie', 'tupac']
+Doublelist = DoubleList()
+for name in sample:
+    Doublelist.add(name)
 
+for name in Doublelist.trasvesral():
+    print(name)
+print(Doublelist.list_size())
 
+Doublelist.add('begin', index=0)
+Doublelist.add('two', index=2)
+Doublelist.add('three', index=4)
+Doublelist.add('six', index=6)
+Doublelist.add('nine', index=9)
 
+for name in Doublelist.trasvesral():
+    print(name)
 
+print(Doublelist.list_size())
 
-
-
-
-
-
-
-
-
-
-
-
-# Test cases
-def test_case(Node):
-    len
-    Node('Spam')
-    Node('Eggs')
-    Node('Ham')
-        
-#test_case(Node)
+print(Doublelist.seacrh('ladi'))
+print(Doublelist.seacrh('ladipo'))
---
+++ sorting.py
@@ -0,0 +1,21 @@
+'''
+sorting
+'''
+# Bubble Sort
+
+
+def bubble_sort(arr):
+    '''' 
+    bubblesort
+    '''
+    len_arr = len(arr) - 1
+    for j in range(len_arr, 0, -1):
+        for i in range(j):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    return
+
+#TEST CASES AREA 
+sample = [2,46,7,32,8,32,4,2,15,67,9]
+bubble_sort(sample)
+print(sample)
